812356377a175ca7474679ddb4a72ba97506c317
Ashley Jain <ashley8jain@gmail.com>
2018-01-27 19:25:07 +0530

    consumer struct solidity

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -6,17 +6,18 @@ contract Store {
   struct Media {
     bytes32 title;
     uint price;
+    address[] consumers;
   }
 
   struct Creator {
-    string[] mediaList;
+    string[] creatorMediaList;
   }
 
   struct Consumer {
-    bytes32[] Media;
+    string[] consumerMediaList;
   }
 
-  string[] allmediaList;
+  string[] allMedia;
 
   mapping (address => uint) public wallets;
   mapping (string => Media) mediaStructs;
@@ -38,8 +39,8 @@ contract Store {
     uint req_cost;
     uint i;
     bytes32 hashcode;
-    for(i = 0; i < creatorStructs[creatorAddress].mediaList.length; i++) {
-      hashcode = creatorStructs[creatorAddress].mediaList[i];
+    for(i = 0; i < creatorStructs[creatorAddress].creatorMediaList.length; i++) {
+      hashcode = creatorStructs[creatorAddress].creatorMediaList[i];
       if (creatorStructs[creatorAddress].mediaStructs[hashcode].title == mname) {
         req_cost = creatorStructs[creatorAddress].mediaStructs[hashcode].price;
         break;
@@ -54,8 +55,8 @@ contract Store {
 
   function addMedia(string code, bytes32 mname, uint cost) view public returns (bytes32, uint) {
     require(validMedia(code));
-    creatorStructs[msg.sender].mediaList.push(code);
-    allmediaList.push(code);
+    creatorStructs[msg.sender].creatorMediaList.push(code);
+    allMedia.push(code);
     mediaStructs[code].title = mname;
     mediaStructs[code].price = cost;
     return (mediaStructs[code].title, mediaStructs[code].price);
@@ -74,17 +75,17 @@ contract Store {
   }
 
   function getMedia(address creatorAddress, uint index) view public returns (string, bytes32, uint) {
-    string url = creatorStructs[creatorAddress].mediaList[index];
+    string url = creatorStructs[creatorAddress].creatorMediaList[index];
     return (url, mediaStructs[url].title, mediaStructs[url].price);
   }
 
   function getMediaCount(address creatorAddress) view public returns (uint) {
-    return creatorStructs[creatorAddress].mediaList.length;
+    return creatorStructs[creatorAddress].creatorMediaList.length;
   }
 
   function validMedia(string code) view public returns (bool) {
-    for(uint i = 0; i < allmediaList.length; i++) {
-      if (sha3(allmediaList[i]) == sha3(code)){
+    for(uint i = 0; i < allMedia.length; i++) {
+      if (sha3(allMedia[i]) == sha3(code)){
         return false;
       }
     }
